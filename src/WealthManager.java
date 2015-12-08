e45c077e9aa7d238d26769f3747292e876f70d4c
MichelleOlivier <michelle.m.olivier@sbcglobal.net>
2015-12-08 09:04:54 -0800

    Create WealthManager.java

---
+++ src/WealthManager.java
@@ -0,0 +1,18 @@
+//package hw03;
+
+/**
+ *
+ * @author Michelle
+ */
+public class WealthManager 
+{
+
+    public static void main(String[] args) 
+    {
+        WealthManagerForm mainForm = new WealthManagerForm();
+        
+        mainForm.setVisible(true);
+        
+       
+    }    
+}
