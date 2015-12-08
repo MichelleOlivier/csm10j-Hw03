379bb43cfb26dc741d75ae990eab06850359d914
MichelleOlivier <michelle.m.olivier@sbcglobal.net>
2015-12-08 09:06:23 -0800

    Create Asset.java

---
+++ WealthManagement/Asset.java
@@ -0,0 +1,13 @@
+//package hw03;
+/**
+ *
+ * @author Michelle
+ */
+abstract public class Asset 
+{
+    String name;
+    abstract public Double getAssetValue();
+    abstract public Double getDebtAmount();
+    abstract public Double getNetValue();
+    
+}
